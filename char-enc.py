4c8a16e1ab704716aa0bb46a9b30bd160bad6d03
RhFz1 <nooralifirst@gmail.com>
2024-05-20 13:32:01 +0530

    Add Functionality and Requirements

--- char-enc.py
+++ char-enc.py
@@ -1,5 +1,32 @@
 import os
+from dotenv import load_dotenv
 
-encode = lambda s : [ord(c) for c in s]
+load_dotenv()
 
-print(encode("abcd"))
+# Importing data paths
+input_text_path = os.path.join(os.environ.get("DATA_PATH"), 'data.txt')
+tokenized_text_path = os.path.join(os.environ.get("DATA_PATH"), 'encodings.txt')
+
+# Reading the text file.
+if not os.path.exists(input_text_path):
+    pass
+
+text = ""
+with open(input_text_path, 'r') as file:
+    text = file.read()
+file.close()
+
+# Encoding the text contents to numbers
+tokenizer = lambda s : ','.join([str(ord(c)) for c in s])
+tokenized_text  = tokenizer(text)
+
+if not os.path.exists(tokenized_text_path):
+    with open(tokenized_text_path, 'w+') as file:
+        file.write(tokenized_text)
+
+# Decoding the encoded numbers
+
+decoder = lambda s : ''.join(chr(int(c)) for c in s.split(','))
+print(decoder(tokenized_text))
+
+
