12bf7a4747e0f6e6b539a866cb650e33aad848c7
RhFz1 <nooralifirst@gmail.com>
2024-07-22 10:24:27 +0530

    Fetch the highest freq. pair

--- bytepair_tok.py
+++ bytepair_tok.py
@@ -5,7 +5,6 @@ load_dotenv()
 
 # Importing data paths
 input_text_path = os.path.join(os.environ.get("DATA_PATH"), 'data.txt')
-tokenized_text_path = os.path.join(os.environ.get("DATA_PATH"), 'tokenized.txt')
 
 # this function will take a list of tokens and return a dictionary with the counts of each token pair
 def get_stats(tokens):
@@ -25,4 +24,10 @@ tokens = list(map(lambda x: int(x), tokens))
 
 counts = get_stats(tokens)
 
-print(sorted(((v , k) for k, v in counts.items()), reverse=True))
+# print(sorted(((v , k) for k, v in counts.items()), reverse=True))
+
+# here im supposed to get the key which has the highest frequency
+
+top_pair = max(counts, key = counts.get)
+
+print(top_pair)
