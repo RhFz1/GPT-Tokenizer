0601edb1c9b8b189cf431cdcb3411c05482a2bee
RhFz1 <nooralifirst@gmail.com>
2024-07-22 10:14:31 +0530

    Change naming conv. and add BPE skeleton

---
+++ bytepair_tok.py
@@ -0,0 +1,28 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Importing data paths
+input_text_path = os.path.join(os.environ.get("DATA_PATH"), 'data.txt')
+tokenized_text_path = os.path.join(os.environ.get("DATA_PATH"), 'tokenized.txt')
+
+# this function will take a list of tokens and return a dictionary with the counts of each token pair
+def get_stats(tokens):
+    counts = {}
+    for pair in zip(text, text[1: ]):
+        counts[pair] = counts.get(pair , 0) + 1
+    
+    return counts
+
+text = ""
+with open(input_text_path, 'r') as file:
+    text = file.read()
+file.close()
+
+tokens = text.encode('utf-8')
+tokens = list(map(lambda x: int(x), tokens))
+
+counts = get_stats(tokens)
+
+print(sorted(((v , k) for k, v in counts.items()), reverse=True))
